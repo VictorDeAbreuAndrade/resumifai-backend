1ba42a018f2151819f06fb949a3f89e1fc8413c0
VictorDeAbreuAndrade <v.andrade@hotmail.com>
2025-12-01 02:38:06 -0300

    Including possibility to get transcription from tiktok videos

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(express.json());
 
 app.post("/", async (req, res) => {
   const videoId = req.body.videoId;
-  const stepByStep = req.body.stepByStep;
+  const url = req.body.url;
+  const mode = req.body.mode;
   const wordLimit = req.body.wordLimit;
 
   console.log("Video ID:", videoId);
@@ -35,26 +36,47 @@ app.post("/", async (req, res) => {
 
   try {
 
-    // Obtaining the transcription
-    const youtube = await Innertube.create({retrieve_player: false})
-    const info = await youtube.getInfo(`${videoId}`);
-    const transcriptData = await info.getTranscript();
+    let transcriptData;
+    let transcript;
 
-    if (!transcriptData) {
-      return res.status(400).json({ error: "Transcription not found!" });
+    // Obtaining the transcription
+    if (url.includes('tiktok.com')) {
+      const transcriptData = await fetch('https://submagic-free-tools.fly.dev/api/tiktok-transcription', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url })
+      })
+
+      if (!transcriptData) {
+        return res.status(400).json({ error: "Transcription not found!" });
+      }
+
+      transcript = Object.values(JSON.parse(transcriptData).transcripts).pop();
+
+    } else {
+      const youtube = await Innertube.create({retrieve_player: false})
+      const info = await youtube.getInfo(`${videoId}`);
+      transcriptData = await info.getTranscript();
+
+      if (!transcriptData) {
+        return res.status(400).json({ error: "Transcription not found!" });
+      }
+
+      const transcript = transcriptData.transcript.content.body.initial_segments.map(
+        (segment) => segment.snippet.text
+      );
     }
 
-    const transcript = transcriptData.transcript.content.body.initial_segments.map(
-      (segment) => segment.snippet.text
-    );
-
-    // console.log(transcript.join(" "));
     console.log("Transcription generated successfully!");
 
     // Generating the summary
     let prompt = ''
-    if (stepByStep) {
+    if (mode == 'StepByStep') {
       prompt = `Make a step-by-step from the text below, keeping the important information. Don't include information about ads and sponsorship. ${wordLimitPhrase}Finally, keep the step-by-step in the same language as the text. That's the text:\n\n${transcript.join(" ")}`;
+    } else if (mode == 'script') {
+      prompt = `You are a famous influencer that make a living by producing viral videos to post on social media like YouTube, TikTok and Instagram. This text below is a transcript from another video. I want you to make a script from that, which will serve to produce a video to post on social media. Change the words, but keep the same meaning. You have to use a striking tone and a biting humor. Start with a viral and impactant phrase, to retain the user attention. Don't include information about ads and sponsorship. ${wordLimitPhrase}Finally, keep the script in the same language as the text. That's the text:\n\n${transcript.join(" ")}`;
     } else {
       prompt = `Sum up the text below, keeping the important information. Don't include information about ads and sponsorship. ${wordLimitPhrase}Finally, keep the summary in the same language as the text. That's the text:\n\n${transcript.join(" ")}`;
     }
@@ -68,7 +90,7 @@ app.post("/", async (req, res) => {
     console.error("Error:", error.message);
     res.status(500).json({
       error:
-        "Error trying to extract the video transcription from a YouTube video.",
+        "Error trying to extract the video transcription from the video.",
     });
   }
 });
